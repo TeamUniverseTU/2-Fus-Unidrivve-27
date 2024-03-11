ecfba57dc4900da583931b7169b76c116dc5183e
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2024-03-11 13:39:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "AstroVerse")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001512057749"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002105109485"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001493699624"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
